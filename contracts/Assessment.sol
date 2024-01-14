aafe8ac30939a8e5a57f585ced1c41617d8fabac
abhinandhan345 <155257877+abhinandhan345@users.noreply.github.com>
2024-01-14 16:02:35 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,41 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public highScore;
+    bool public bountyClaimed;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event HighScoreSet(uint256 score);
+    event BountyClaimed(address claimer, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        highScore = 0;
+        bountyClaimed = false;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,15 +42,26 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
-}
 
+    function setHighScore(uint256 _score) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_score > highScore, "New score must be higher than the current high score");
+        highScore = _score;
+        emit HighScoreSet(_score);
+    }
+
+    function claimBounty() public {
+        require(!bountyClaimed, "Bounty already claimed");
+        require(msg.sender == owner, "You are not the owner of this account");
+        
+        uint256 bountyAmount = balance;
+        balance = 0;
+        bountyClaimed = true;
+        owner.transfer(bountyAmount);
+        emit BountyClaimed(msg.sender, bountyAmount);
+    }
+}
